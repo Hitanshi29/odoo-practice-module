236a0ac54878ee6dce8034d1311c25671fbdcfbb
Hitanshi29 <hitanshirana2912@gmail.com>
2026-04-24 16:37:19 +0530

    Add module

---
+++ sale_customer_practice/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo Module Developed by Bizople Solutions Pvt. Ltd.
+# See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Sale Customer Practice',
+    'version': '1.0',
+    'author': 'Bizople Solutions Pvt. Ltd.',
+    'website': 'https://www.bizople.com',
+    'summary': '',
+    'description': "",
+    'depends': ['sale_management'],
+    'data': [
+           ],
+    'sequence': 1,
+    'application': True,
+    'installable': True,
+    'license': 'LGPL-3',
+}
